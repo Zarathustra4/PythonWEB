a790bc796d9fd598647bedc13f570adfee8cd439
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-10-19 15:19:44 +0300

    Footer info is configured

--- Lab3/app.py
+++ Lab3/app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import platform
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    return render_template("index.html",
+                           os_name=platform.system(),
+                           user_agent=request.user_agent)
 
 
 @app.route("/hobbies")
 def hobbies_page():
-    return render_template("hobbies.html")
+    return render_template("hobbies.html",
+                           os_name=platform.system(),
+                           user_agent=request.user_agent)
 
 
 @app.route("/study")
 def study_page():
-    return render_template("studying.html")
+    return render_template("studying.html",
+                           os_name=platform.system(),
+                           user_agent=request.user_agent)
 
 
