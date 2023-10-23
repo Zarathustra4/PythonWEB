84c025a3fc3600542d1cd7c8fc6f785aa4ae8186
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-10-23 12:48:20 +0300

    Login page is completed

--- Lab4/app/views.py
+++ Lab4/app/views.py
@@ -1,10 +1,11 @@
 from app import app
+from app import auth
 from flask import render_template, request
 import platform
 import datetime
 
-
 SKILLS = ["C++", "Python", "Java", "Spring", "Math", "SQL", "REST API", "Git", "Linux", "HTML/CSS"]
+
 
 @app.route("/")
 def main_page():
@@ -47,3 +48,23 @@ def skills_page(id=None):
                            len=len(skills),
                            is_list=is_list,
                            skills=skills)
+
+
+@app.route("/login", methods=['GET'])
+def login_page():
+    return render_template("login.html",
+                           os_name=platform.system(),
+                           user_agent=request.user_agent,
+                           time=datetime.datetime.now())
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    login_input = request.form.get('login')
+    password_input = request.form.get('password')
+    if auth.authenticate(login_input, password_input):
+        return render_template("index.html", os_name=platform.system(),
+                               user_agent=request.user_agent,
+                               time=datetime.datetime.now())
+    else:
+        return render_template("error.html", message="Wrong login or password")
