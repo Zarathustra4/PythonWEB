8a24968c2fad67d63a24c9c72dbf0622411395f3
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-11-02 12:24:06 +0200

    bugfix

--- Samost/app/domain/models.py
+++ Samost/app/domain/models.py
@@ -4,6 +4,3 @@ class ReviewModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     message = db.Column(db.String(500), nullable=False)
-
-with app.app_context():
-    db.create_all()
