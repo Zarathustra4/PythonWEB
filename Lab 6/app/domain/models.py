482fdbce1e3dd214889e1162652099518b32883b
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-11-06 11:21:26 +0200

    init

---
+++ Lab 6/app/domain/models.py
@@ -0,0 +1,6 @@
+from app import db, app
+
+class ReviewModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
