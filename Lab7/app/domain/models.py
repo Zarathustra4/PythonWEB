25c515c54ee841c7ab0ba840698f61e657f0fd46
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-11-09 20:16:31 +0200

    migrations are done

--- Lab7/app/domain/models.py
+++ Lab7/app/domain/models.py
@@ -1,4 +1,4 @@
-from app import db, app
+from app import db
 from enum import Enum
 
 
@@ -13,3 +13,13 @@ class TodoModel(db.Model):
     todo = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Enum(StatusEnum))
 
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(125), unique=True, nullable=False)
+    email = db.Column(db.String(125), unique=True, nullable=False)
+    image_file = db.Column(db.String(125), nullable=False, default="default.jpg")
+    password = db.Column(db.String(125), nullable=False)
+
+    def __repr__(self):
+        return f"( {self.username}, {self.email} )"
---
+++ Lab7/config.py
@@ -0,0 +1,6 @@
+import os.path
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+WTF_CSRF_ENABLED = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'labs.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
